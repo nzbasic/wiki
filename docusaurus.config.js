21ee54b1900c85492f9f9fed04c7f88c2d76b841
nzbasic <jcop589@aucklanduni.ac.nz>
2021-03-14 10:59:20 +1300

    configure docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,8 +21,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl:
-            "https://github.com/facebook/docusaurus/edit/master/website/",
+          editUrl: "https://github.com/nzbasic/wiki/edit/master/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
